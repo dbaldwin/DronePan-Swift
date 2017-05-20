b8b3757f8e3dea55ed0b6fdea24917e774824a47
Dennis Baldwin <dennisbaldwin@gmail.com>
2017-05-19 19:23:58 -0500

    Getting connected to aircraft and setting up camera delegate

--- DronePan-Swift/ViewController.swift
+++ DronePan-Swift/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import DJISDK
+import VideoPreviewer
 
 class ViewController: UIViewController {
     
@@ -30,6 +31,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fetchCamera() -> DJICamera? {
+        
+        if DJISDKManager.product() == nil {
+            return nil
+        }
+        
+        if DJISDKManager.product() is DJIAircraft {
+            return (DJISDKManager.product() as! DJIAircraft).camera
+        } else if DJISDKManager.product() is DJIHandheld {
+            return (DJISDKManager.product() as! DJIHandheld).camera
+        }
+        
+        return nil
+    }
 
 }
 
@@ -51,7 +66,22 @@ extension ViewController: DJISDKManagerDelegate {
         
     }
     
+    // Called when we establish a connection with the aircraft
     func productConnected(_ product: DJIBaseProduct?) {
+        
+        guard let newProduct = DJISDKManager.product() else {
+            print("Product is connected but DJISDKManager.product is nil -> something is wrong")
+            return;
+        }
+        
+        print("Product is connected: \(newProduct.model)")
+        
+        let camera: DJICamera? = fetchCamera()
+        
+        // Setup video feed
+        /*VideoPreviewer.instance().setView(cameraView)
+        DJISDKManager.videoFeeder()?.primaryVideoFeed.add(self, with: nil)
+        VideoPreviewer.instance().start()*/
         
     }
     
